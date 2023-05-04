553bfbcca70e332dabf49d18488f3a349093ef1c
cdawson-73 <callum.j.dawson@hotmail.co.uk>
2023-05-04 01:21:47 +0100

    Initial commit.

---
+++ models/achievementModel.js
@@ -0,0 +1,84 @@
+const achievements = {
+        oneGenSet: {
+            name: "",
+            image: "/img/achievements/general/set/1-general-set.png",
+            type: "general",
+            achieved: "false",
+            dateAchieved: "null",
+            description: "",
+        },
+        /*tenGenSet: {
+
+        },
+        twenGenSet: {
+
+        },
+        oneGenCom: {
+
+        },
+        tenGenCom: {
+
+        },
+        twenGenCom: {
+
+        },
+        oneFitSet: {
+
+        },
+        tenFitSet: {
+
+        },
+        twenFitSet: {
+
+        },
+        oneFitCom: {
+
+        },
+        tenFitCom: {
+
+        },
+        twenFitCom: {
+
+        },
+        oneHealSet: {
+
+        },
+        tenHealSet: {
+
+        },
+        twenHealSet: {
+
+        },
+        oneHealCom: {
+
+        },
+        tenHealCom: {
+
+        },
+        twenHealCom: {
+
+        },
+        oneNutSet: {
+
+        },
+        tenNutSet: {
+
+        },
+        twenNutSet: {
+
+        },
+        oneNutCom: {
+
+        },
+        tenNutCom: {
+
+        },
+        twenNutCom: {
+
+        },
+        final: {
+
+        }*/
+    }
+
+    module.exports = achievements;
