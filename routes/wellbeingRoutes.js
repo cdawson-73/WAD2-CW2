20cb1927d3a9dd015e6236dbc6a7b568d8db2b34
cdawson-73 <callum.j.dawson@hotmail.co.uk>
2023-05-07 22:39:37 +0100

    Added routes for complete, edit, delete goals.

--- routes/wellbeingRoutes.js
+++ routes/wellbeingRoutes.js
@@ -18,13 +18,33 @@ router.get("/signup", loginCheck.ensureLoggedOut("/:username/account"), controll
 router.post("/signup", controller.post_new_user);
 
 // User only pages.
+
+// Account
 router.get("/:username/account", loginCheck.ensureLoggedIn("/login"), controller.show_account);
+
+// Achievements
 router.get("/:username/achievements", loginCheck.ensureLoggedIn("/login"), controller.show_achievements);
 router.get("/:username/achievement/:name", loginCheck.ensureLoggedIn("/login"), controller.show_achievement);
+
+// Add Goal
 router.get("/:username/add-goal", loginCheck.ensureLoggedIn("/login"), controller.show_new_goal);
-router.post("/:username/add-goal", controller.post_new_goal);
+router.post("/:username/add-goal", loginCheck.ensureLoggedIn("/login"), controller.post_new_goal);
+
+// Show Goals
 router.get("/:username/goals", loginCheck.ensureLoggedIn("/login"), controller.show_goals);
 router.get("/:username/goal/:id", loginCheck.ensureLoggedIn("/login"), controller.show_goal);
+
+// Edit Goals
+router.get("/:username/edit-goal/:id", loginCheck.ensureLoggedIn("/login"), controller.show_edit_goal);
+router.post("/:username/edit-goal/:id", loginCheck.ensureLoggedIn("/login"), controller.post_edit_goal);
+
+// Complete Goal
+router.get("/:username/complete-goal/:id", loginCheck.ensureLoggedIn("/login"), controller.show_complete_goal);
+router.post("/:username/complete-goal/:id", loginCheck.ensureLoggedIn("/login"), controller.post_complete_goal);
+
+// Delete Goal
+router.get("/:username/delete-goal/:id", loginCheck.ensureLoggedIn("/login"), controller.show_delete_goal);
+router.post("/:username/delete-goal/:id", loginCheck.ensureLoggedIn("/login"), controller.post_delete_goal);
 
 // Log out page.
 router.get("/logout", controller.logout);
